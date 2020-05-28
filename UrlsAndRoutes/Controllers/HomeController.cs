edbbbb3ee4f7c1d60eda2250bcd29a1f7084ffe0
Austin D'Amico <adamico2412@gmail.com>
2020-05-27 20:19:14 -0400

    UrlsAndRoutes demo

---
+++ UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UrlsAndRoutes.Models;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View("Result",
+            new Result
+            {
+                Controller = nameof(HomeController),
+                Action = nameof(Index)
+            });
+    }
+}
